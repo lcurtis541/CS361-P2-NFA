356fed7ca05442693d3f5307741acdebddfb856d
lcurtis541 <70545873+lcurtis541@users.noreply.github.com>
2021-11-16 19:53:18 -0700

    Implemented part of NFA
    Got the first few methods of NFA done

--- fa/nfa/NFA.java
+++ fa/nfa/NFA.java
@@ -1,9 +1,11 @@
 package fa.nfa;
 
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 import fa.State;
 import fa.dfa.DFA;
+import fa.dfa.DFAState;
 
 /**
  * This simulates a Nondeterministic Finite Autonoma
@@ -11,23 +13,64 @@ import fa.dfa.DFA;
  *
  */
 public class NFA implements NFAInterface {
+	
+	private Set<NFAState> states;
+	private String start;
+	private Set<Character> ordAbc;
+
+	public NFA(){
+		states = new LinkedHashSet<NFAState>();
+		ordAbc = new LinkedHashSet<Character>();
+	}
 
 	@Override
 	public void addStartState(String name) {
-		// TODO Auto-generated method stub
+		NFAState n = checkIfExists(new NFAState(name));
+		if(n == null){;
+			addState(name);
+		} else {
+			System.out.println("WARNING: A state with name " + name + " already exists in the DFA");
+		}
+		start = name;
 		
+	}
+
+	/**
+	 * Check if a state with such name already exists
+	 * @param name
+	 * @return null if no state exist, or SFAState object otherwise.
+	 */
+	private NFAState checkIfExists(NFAState name) {
+		NFAState ret = null;
+		for(NFAState s : states){
+			if(s.getName().equals(name.getName())){
+				ret = s;
+				break;
+			}
+		}
+		return ret;
 	}
 
 	@Override
 	public void addState(String name) {
-		// TODO Auto-generated method stub
-		
+		NFAState s = new NFAState(name);
+		NFAState n = checkIfExists(s);
+		if( n == null){
+			states.add(s);
+		} else {
+			System.out.println("WARNING: A state with name " + name + " already exists in the DFA");
+		}
 	}
 
 	@Override
 	public void addFinalState(String name) {
-		// TODO Auto-generated method stub
-		
+		NFAState s = new NFAState(name,true);
+		NFAState n = checkIfExists(s);
+		if( n == null){
+			states.add(s);
+		} else {
+			System.out.println("WARNING: A state with name " + name + " already exists in the DFA");
+		}
 	}
 
 	@Override
