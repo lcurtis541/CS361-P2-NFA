2e8d68bed1b3bf957dcdf5c04ed5620a87f02db9
lcurtis541 <70545873+lcurtis541@users.noreply.github.com>
2021-11-16 19:20:23 -0700

    Added NFA and NFAState
    We will need to fill out NFA and NFAState files to complete the project

---
+++ fa/nfa/NFAState.java
@@ -0,0 +1,12 @@
+package fa.nfa;
+
+import fa.State;
+
+/**
+ * This simulates a state within a Nondeterministic Finite Autonoma
+ * @author Landon and Daniel
+ *
+ */
+public class NFAState extends State {
+
+}
